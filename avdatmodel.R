c9a1f7b71393ec18cf3e2208371608b8be942bdc
David Lawrence Miller <dill@mapc-0178.campus.bath.ac.uk>
2010-04-14 14:26:30 +0100

    silly mistake in mudging... running out of memory though!!

--- avdatmodel.R
+++ avdatmodel.R
@@ -20,6 +20,9 @@ fixdat<-fix_it_data(full)
 # load averaged data
 av.dat<-read.csv(file="av.dat")
 
+av.dat$year<-as.numeric(av.dat$year)
+av.dat$share_100<-av.dat$share_100+1e-15
+
 # Italy boundary
 it<-list(x=fixdat$italy$map$km.e,y=fixdat$italy$map$km.n)
 
@@ -27,14 +30,24 @@ it<-list(x=fixdat$italy$map$km.e,y=fixdat$italy$map$km.n)
 soap.knots<-make_soap_grid(list(x=fixdat$italy$map$km.e,y=fixdat$italy$map$km.n),c(20,30))
 soap.knots<-pe(soap.knots,-c(11,32,47,121))
 
+# for 40x60
+#soap.knots<-pe(soap.knots,-c(1,4,27,28,33,34,53,159,179,231,284,285,296,450,488,510,556,567,582))
+
+
+
 onoff<-inSide(it,av.dat$x,av.dat$y)
 
 av.dat<-pe(av.dat,onoff)
 
 # run a model!
-b.soap<- gam(share_100~
+b.soap<- bam(share_100~
    te(x,y,year,bs=c("sf","cr"),k=c(100,4),d=c(2,1),xt=list(list(bnd=list(it)),NULL))+
    te(x,y,year,bs=c("sw","cr"),k=c(100,4),d=c(2,1),xt=list(list(bnd=list(it)),NULL))
             ,knots=soap.knots,data=av.dat,family=Gamma(link="log"))
 
+#b.soap<- gam(share_100~
+#   te(x,y,year,bs=c("sf","cr"),k=c(100,4),d=c(2,1),xt=list(list(bnd=list(it)),NULL))+
+#   te(x,y,year,bs=c("sw","cr"),k=c(100,4),d=c(2,1),xt=list(list(bnd=list(it)),NULL))
+#            ,knots=soap.knots,data=av.dat)
+#
 
