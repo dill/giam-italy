76dbf01d1e7a87c04a94d0c16851858be7b463c0
David Lawrence Miller <dave@ninepointeightone.net>
2010-09-01 14:08:04 +0100

    ...

--- smoothtrends.R
+++ smoothtrends.R
@@ -11,68 +11,8 @@ require(splines)
 load("REMLfull.RData")
 
 # load the trend data...
-load("trend.RData")
-
-
-######### put the Sardinia and Sicily stuff in
-
-# Sardinia
-set.seed(1)
-
-year[[1]]<-av.dat.sa$year==2003
-year[[2]]<-av.dat.sa$year==2004
-year[[3]]<-av.dat.sa$year==2005
-year[[4]]<-av.dat.sa$year==2006
-year[[5]]<-av.dat.sa$year==2007
-year[[6]]<-av.dat.sa$year==2008
-
-b<-sa.soap
-# extract the model matrix
-Xb<-model.matrix(b)
-
-for( i in 1:Nb){
-   # simulate \theta_b
-   bs  <- rmvnorm(1, mean = b$coeff, sigma=b$Vp, method=s.meth)
-   # calculate the expected value
-   ex<-exp(Xb%*%t(bs))
-
-   for(j in 1:6){
-      # full
-      RES[j,i]<-mean(c(RES[j,i],ex[year[[j]],]))
-      # south
-      RES[j+18,i]<-mean(c(RES[j+18,i],ex[year[[j]],]))
-   }
-}
-
-# Sicily
-set.seed(1)
-
-year[[1]]<-av.dat.sc$year==2003
-year[[2]]<-av.dat.sc$year==2004
-year[[3]]<-av.dat.sc$year==2005
-year[[4]]<-av.dat.sc$year==2006
-year[[5]]<-av.dat.sc$year==2007
-year[[6]]<-av.dat.sc$year==2008
-
-b<-sc.soap
-# extract the model matrix
-Xb<-model.matrix(b)
-
-for( i in 1:Nb){
-   # simulate \theta_b
-   bs  <- rmvnorm(1, mean = b$coeff, sigma=b$Vp, method=s.meth)
-   # calculate the expected value
-   ex<-exp(Xb%*%t(bs))
-
-   for(j in 1:6){
-      # full
-      RES[j,i]<-mean(c(RES[j,i],ex[year[[j]],]))
-      # south
-      RES[j+18,i]<-mean(c(RES[j+18,i],ex[year[[j]],]))
-   }
-}
-
-
+#load("trend.RData")
+load("fulltrend.RData")
 
 
 ##############################################
@@ -80,7 +20,7 @@ for( i in 1:Nb){
 # plot stuff
 par(mfrow=c(2,2),las=1,mar=c(3.5,3,2,0.75),mgp=c(2,0.65,0))
 
-titles<-c("Mainland Italy","North","Centre","South")
+titles<-c("Italy","North","Centre","South")
 
 j<-1
 
