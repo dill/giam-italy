1ec8c0cce16e80d65ef8b4bf079f6c2f104263cb
David Lawrence Miller <dave@ninepointeightone.net>
2010-10-07 22:54:17 +0100

    noavmodel ready to run

--- noavmodel/model.R
+++ noavmodel/model.R
@@ -13,183 +13,145 @@ source("fixit.R")
 full<-read.csv(file="database_complete.csv")
 fixdat<-fix_it_data(full)
 
-# try tweedie errors!!!
-#av.dat$year<-as.numeric(av.dat$year)
-#av.dat$share_100<-av.dat$share_100+1e-15
-#av.dat$share_100<-av.dat$share_100+1e-5
-
-
 ########################
 # Italy
-
 # Italy boundary
 it<-list(x=fixdat$italy$map$km.e,y=fixdat$italy$map$km.n)
 # data
 it.dat<-list(x=fixdat$italy$dat$km.e,
              y=fixdat$italy$dat$km.n,
              year=fixdat$italy$dat$year,
-             share_100=fixdat$italy$dat$share_100)
+             share_100=fixdat$italy$dat$share_100)#+1e-15)
 
 # setup the soap knots
-#20x25
-soap.knots<-make_soap_grid(it,c(20,25))
-soap.knots<-pe(soap.knots,-c(4,5,11,35,61,68,108))
+soap.knots<-make_soap_grid(it,c(15,15))
+#soap.knots<-pe(soap.knots,-c(4,5,11,35,61,68,108)) #20 x 25
+soap.knots<-pe(soap.knots,-c(1,46)) #15 x15
 
 # basis size
-it.bsize<-c(20,6)
+it.bsize<-c(20,12)
 
 it.soap<- gam(share_100~
    te(x,y,year,bs=c("sf","cr"),k=it.bsize,d=c(2,1),xt=list(list(bnd=list(it)),NULL))+
    te(x,y,year,bs=c("sw","cr"),k=it.bsize,d=c(2,1),xt=list(list(bnd=list(it)),NULL))
-            ,knots=soap.knots,data=it.dat,family=Gamma(link="log"),method="REML")
-#it.soap<- gam(share_100~
-#   te(x,y,year,bs=c("sf","cr"),k=it.bsize,d=c(2,1),xt=list(list(bnd=list(it)),NULL))+
-#   te(x,y,year,bs=c("sw","cr"),k=it.bsize,d=c(2,1),xt=list(list(bnd=list(it)),NULL))
-#            ,knots=soap.knots,data=it.dat,family=Tweedie(link=power(0),p=1.5),method="REML")
+#            ,knots=soap.knots,data=it.dat,family=Tweedie(link=power(1),p=1.6),method="REML")
+            ,knots=soap.knots,data=it.dat,family=Tweedie(link=power(0),p=1.6),method="REML")
+#            ,knots=soap.knots,data=it.dat,family=Gamma(link="log"),method="REML")
 ##########################
 gc()
 
 save.image("it.RData")
 
-########################
-# Sardinia 
-
-# Sardinia boundary
-sa<-list(x=fixdat$sardinia$map$km.e,y=fixdat$sardinia$map$km.n)
-# data
-sa.dat<-list(x=fixdat$sardinia$dat$km.e,
-             y=fixdat$sardinia$dat$km.n,
-             year=fixdat$sardinia$dat$year,
-             share_100=fixdat$sardinia$dat$share_100)
-
-# setup the soap knots
-#20x25
-soap.knots<-make_soap_grid(sa,c(10,10))
-soap.knots<-pe(soap.knots,-c(47))
-
-sa.ksize<-c(10,4)
-
-sa.soap<- gam(share_100~
-   te(x,y,year,bs=c("sf","cr"),k=sc.ksize,d=c(2,1),xt=list(list(bnd=list(sa)),NULL))+
-   te(x,y,year,bs=c("sw","cr"),k=sc.ksize,d=c(2,1),xt=list(list(bnd=list(sa)),NULL))
-            ,knots=soap.knots,data=av.dat.sa,family=Gamma(link="log"),method="REML")
+#########################
+## Sardinia 
+#
+## Sardinia boundary
+#sa<-list(x=fixdat$sardinia$map$km.e,y=fixdat$sardinia$map$km.n)
+## data
+#sa.dat<-list(x=fixdat$sardinia$dat$km.e,
+#             y=fixdat$sardinia$dat$km.n,
+#             year=fixdat$sardinia$dat$year,
+#             share_100=fixdat$sardinia$dat$share_100)
+#
+## setup the soap knots
+##20x25
+#soap.knots<-make_soap_grid(sa,c(10,10))
+#soap.knots<-pe(soap.knots,-c(47))
+#
+#sa.ksize<-c(10,4)
+#
 #sa.soap<- gam(share_100~
-#   te(x,y,year,bs=c("sf","cr"),k=sa.ksize,d=c(2,1),xt=list(list(bnd=list(sa)),NULL))+
-#   te(x,y,year,bs=c("sw","cr"),k=sa.ksize,d=c(2,1),xt=list(list(bnd=list(sa)),NULL))
-#            ,knots=soap.knots,data=av.dat.sa,family=Tweedie(link=power(0),p=1.5),method="REML")
-##########################
-gc()
-
-save.image("sa.RData")
-
-
-########################
-# Sicily 
-
-# Sicily boundary
-sc<-list(x=fixdat$sicily$map$km.e,y=fixdat$sicily$map$km.n)
-# data
-sc.dat<-list(x=fixdat$sicily$dat$km.e,
-             y=fixdat$sicily$dat$km.n,
-             year=fixdat$sicily$dat$year,
-             share_100=fixdat$sicily$dat$share_100)
-
-# setup the soap knots
-soap.knots<-make_soap_grid(sc,c(10,10))
-#soap.knots<-pe(soap.knots,-c(4,5,11,35,61,68,108))
-
-sc.bsize<-c(10,4)
-sc.soap<- gam(share_100~
-   te(x,y,year,bs=c("sf","cr"),k=c(20,4),d=c(2,1),xt=list(list(bnd=list(sc)),NULL))+
-   te(x,y,year,bs=c("sw","cr"),k=c(20,4),d=c(2,1),xt=list(list(bnd=list(sc)),NULL))
-            ,knots=soap.knots,data=av.dat.sc,family=Gamma(link="log"),method="REML")
+#   te(x,y,year,bs=c("sf","cr"),k=sc.ksize,d=c(2,1),xt=list(list(bnd=list(sa)),NULL))+
+#   te(x,y,year,bs=c("sw","cr"),k=sc.ksize,d=c(2,1),xt=list(list(bnd=list(sa)),NULL))
+#            ,knots=soap.knots,data=av.dat.sa,family=Gamma(link="log"),method="REML")
+##sa.soap<- gam(share_100~
+##   te(x,y,year,bs=c("sf","cr"),k=sa.ksize,d=c(2,1),xt=list(list(bnd=list(sa)),NULL))+
+##   te(x,y,year,bs=c("sw","cr"),k=sa.ksize,d=c(2,1),xt=list(list(bnd=list(sa)),NULL))
+##            ,knots=soap.knots,data=av.dat.sa,family=Tweedie(link=power(0),p=1.5),method="REML")
+###########################
+#gc()
+#
+#save.image("sa.RData")
+#
+#
+#########################
+## Sicily 
+#
+## Sicily boundary
+#sc<-list(x=fixdat$sicily$map$km.e,y=fixdat$sicily$map$km.n)
+## data
+#sc.dat<-list(x=fixdat$sicily$dat$km.e,
+#             y=fixdat$sicily$dat$km.n,
+#             year=fixdat$sicily$dat$year,
+#             share_100=fixdat$sicily$dat$share_100)
+#
+## setup the soap knots
+#soap.knots<-make_soap_grid(sc,c(10,10))
+##soap.knots<-pe(soap.knots,-c(4,5,11,35,61,68,108))
+#
+#sc.bsize<-c(10,4)
 #sc.soap<- gam(share_100~
-#   te(x,y,year,bs=c("sf","cr"),k=sc.bsize,d=c(2,1),xt=list(list(bnd=list(sc)),NULL))+
-#   te(x,y,year,bs=c("sw","cr"),k=sc.bsize,d=c(2,1),xt=list(list(bnd=list(sc)),NULL))
-#            ,knots=soap.knots,data=av.dat.sc,family=Tweedie(link=power(0),p=1.5),method="REML")
-##########################
-gc()
-
-save.image("sc.RData")
-
-
-pdf(file="maps.pdf",width=9)
+#   te(x,y,year,bs=c("sf","cr"),k=c(20,4),d=c(2,1),xt=list(list(bnd=list(sc)),NULL))+
+#   te(x,y,year,bs=c("sw","cr"),k=c(20,4),d=c(2,1),xt=list(list(bnd=list(sc)),NULL))
+#            ,knots=soap.knots,data=av.dat.sc,family=Gamma(link="log"),method="REML")
+##sc.soap<- gam(share_100~
+##   te(x,y,year,bs=c("sf","cr"),k=sc.bsize,d=c(2,1),xt=list(list(bnd=list(sc)),NULL))+
+##   te(x,y,year,bs=c("sw","cr"),k=sc.bsize,d=c(2,1),xt=list(list(bnd=list(sc)),NULL))
+##            ,knots=soap.knots,data=av.dat.sc,family=Tweedie(link=power(0),p=1.5),method="REML")
+###########################
+#gc()
+#
+#save.image("sc.RData")
+#
+#
+#pdf(file="maps.pdf",width=9)
 ########################
 # now make the image plot
 
 # options
-grid.res<-100
-years<-as.numeric(levels(as.factor(av.dat$year)))
+grid.res.x<-100
+grid.res.y<-60
+years<-as.numeric(levels(as.factor(it.dat$year)))
 
 # setup the prediction grid
-itmat<-matrix(c(av.dat$x,av.dat$y),length(av.dat$x),2)
-it.asc<-ascgen(itmat,nrcol=grid.res)
+xmin<-min(it$x)
+ymin<-min(it$y)
+xmax<-max(it$x)
+ymax<-max(it$y)
+xm <- seq(xmin,xmax,length=grid.res.x);yn<-seq(ymin,ymax,length=grid.res.y)
+xx <- rep(xm,grid.res.y);yy<-rep(yn,rep(grid.res.x,grid.res.y))
 
-# now extract the grid
-gridcuts<-attr(it.asc,"dimnames")
-gridcuts$x<-gsub("\\(","",gridcuts$x)
-gridcuts$x<-gsub("\\]","",gridcuts$x)
-gridcuts$y<-gsub("\\(","",gridcuts$y)
-gridcuts$y<-gsub("\\]","",gridcuts$y)
+im.mat<-matrix(NA,length(years),grid.res.x*grid.res.y)
 
-gridcuts$x<-t(matrix(as.numeric(unlist(strsplit(gridcuts$x,",",extended=TRUE)),2,grid.res),2,grid.res))
-gridcuts$y<-t(matrix(as.numeric(unlist(strsplit(gridcuts$y,",",extended=TRUE)),2,grid.res),2,grid.res))
 
-x.start<-gridcuts$x[,1]
-x.stop <-gridcuts$x[,2]
-y.start<-gridcuts$y[,1]
-y.stop <-gridcuts$y[,2]
+for (i in 1:length(years)){
+   pred.grid<-list(x=xx,y=yy,year=rep(years[i],length(xx)))
+   # italy
+   im.mat[i,]<-predict(it.soap,pred.grid,type="response")
+}
 
-xm<-(x.start+x.stop)/2
-yn<-(y.start+y.stop)/2
-xx <- rep(xm,grid.res)
-yy<-rep(yn,rep(grid.res,grid.res))
+   
+   xlim<-c(xm[1]-25,xm[length(xm)])
+   ylim<-c(yn[1]-25,yn[length(yn)]+25)
+   zlim<-c(0,12)
 
-pred.grid<-list(x=xx,y=yy,year=rep(2003,length(xx)))
-
-im.mat<-matrix(NA,grid.res,grid.res)
 
 par(mfrow=c(2,3),mar=c(4.5,4.5,2,2))
 
-for (year in years){
 
-   pred.grid<-list(x=xx,y=yy,year=rep(year,length(xx)))
-
-   im.it<-im.mat
-
-   # sardinia
-   z<-predict(sa.soap,pred.grid,type="response")
-   im.it[inSide(sa,xx,yy)]<-z[!is.na(z)]
-
-   # sicily
-   z<-predict(sc.soap,pred.grid,type="response")
-   im.it[inSide(sc,xx,yy)]<-z[!is.na(z)]
-
-   # italy
-   z<-predict(it.soap,pred.grid,type="response")
-   im.it[inSide(it,xx,yy)]<-z[!is.na(z)]
-
-
-   im.it<-im.it[1:(grid.res-sum(x.start>620)),]
-   xs<-xm[1:(grid.res-sum(x.start>620))]
-   ys<-yn
-   
-   xlim<-c(xs[1]-25,xs[length(xs)])
-   ylim<-c(ys[1]-25,ys[length(ys)]+25)
-   zlim<-c(0,12)
-
-   image(z=im.it,x=xs,y=ys,
+for (i in 1:length(years)){
+   image(z=matrix(im.mat[i,],grid.res.x,grid.res.y),x=xm,y=yn,
          col=heat.colors(100),xlab="km (e)",ylab="km (n)",
-         main=year,asp=1,cex.main=1.4,
-         cex.lab=1.4,cex.axis=1.3,xlim=xlim,ylim=ylim,zlim=zlim)
+         main=years[i],asp=1,cex.main=1.4,
+         cex.lab=1.4,cex.axis=1.3,zlim=zlim,xlim=xlim,ylim=ylim)
 
-   contour(xs,ys,im.it,levels=seq(zlim[1],zlim[2],by=1),col="blue",add=TRUE)
-
+   contour(xm,yn,matrix(im.mat[i,],grid.res.x,grid.res.y),
+            levels=seq(zlim[1],zlim[2],by=1),col="blue",add=TRUE)
    lines(it,lwd=2)
-   lines(sa,lwd=2)
-   lines(sc,lwd=2)
 
 }
 
+x11()
+gam.check(it.soap)
 
-dev.off()
+#dev.off()
